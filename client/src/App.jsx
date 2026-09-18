ddec3619e0c0257e9a40cc347f2f054f26303f2d
Namit Malasi <namitmalasi@gmail.com>
2026-09-18 18:34:32 +0530

    chore: initialize InvoiceFlow project structure

---
+++ client/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-red-500 text-3xl">App</div>;
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    success: true,
+    message: "InvoiceFlow API is running",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
